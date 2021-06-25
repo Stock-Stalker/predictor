8471edaafb15e40c893b444c206ad023d46ff532
Starlight Romero <starlightromero@protonmail.com>
2021-06-25 11:31:22 -0700

    [create] tests/test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,32 @@
+"""Test predictor/utils.py functions."""
+import json
+import unittest
+import datetime as dt
+from utils import *
+
+
+class TestUtils(unittest.TestCase):
+    """Utils test class."""
+
+    def test_get_today_epoch(self):
+        """Test get today epoch util."""
+        pass
+
+    def test_get_ticker_from_name(self):
+        """Test get ticker from name util."""
+        pass
+
+    def test_get_stop_news_today(self):
+        """Test top news today util."""
+        pass
+
+    def test_get_top_tweets(self):
+        """Test get top tweets util."""
+
+    def test_get_top25_news(self):
+        """Test get top25 news util."""
+        pass
+
+    def test_predictor(self):
+        """Test predictor util."""
+        pass
